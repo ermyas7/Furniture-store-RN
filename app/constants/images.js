4eb677a2db0b144a90dcb65575a91c615334221b
ermyas7 <ermyas.fekadu@gmail.com>
2021-01-19 11:15:34 +0300

    initial commit

---
+++ app/constants/images.js
@@ -0,0 +1,11 @@
+export const sofa = require('../assets/images/sofa.png');
+export const redChair = require('../assets/red_chair.png');
+export const greenChair = require('../assets/images/green_chair.png');
+export const whiteChair = require('../assets/images/white_chair.png');
+
+export default {
+  sofa,
+  redChair,
+  greenChair,
+  whiteChair,
+};
