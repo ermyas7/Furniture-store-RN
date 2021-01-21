0b90376f8d56495db190ed086f061adb56c0da0d
ermyas7 <ermyas.fekadu@gmail.com>
2021-01-21 20:30:20 +0300

    Add title

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -7,24 +7,51 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import {COLORS, icons, SIZES} from '../constants';
+import {COLORS, icons, SIZES, FONTS} from '../constants';
 
 const Home = () => {
   const renderHeader = () => {
     return (
       <View style={{paddingHorizontal: SIZES.padding}}>
         <View style={{flexDirection: 'row'}}>
-          <TouchableOpacity onPress={() => console.log('pressed')} style={{flex: 1}}>
-            <Image source={icons.menu} style={{width: 25,height: 25}} resizeMode="contain"/>
+          <TouchableOpacity
+            onPress={() => console.log('pressed')}
+            style={{flex: 1}}>
+            <Image
+              source={icons.menu}
+              style={{width: 25, height: 25}}
+              resizeMode="contain"
+            />
           </TouchableOpacity>
-          <TouchableOpacity onPress={() => console.log('pressed')} style={{flex: 1, alignItems: 'flex-end'}}>
-            <Image source={icons.cart} style={{width: 25,height: 25}} resizeMode="contain"/>
+          <TouchableOpacity
+            onPress={() => console.log('pressed')}
+            style={{flex: 1, alignItems: 'flex-end'}}>
+            <Image
+              source={icons.cart}
+              style={{width: 25, height: 25}}
+              resizeMode="contain"
+            />
           </TouchableOpacity>
         </View>
       </View>
     );
   };
-  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
+  const renderTitle = (title) => {
+    return (
+      <View style={{marginTop: SIZES.padding, marginHorizontal: SIZES.padding}}>
+        <Text style={{color: COLORS.black, ...FONTS.largeTitle}}>
+          Collection of
+        </Text>
+        <Text style={{color: COLORS.black, ...FONTS.largeTitle}}>{title}</Text>
+      </View>
+    );
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      {renderHeader()}
+      {renderTitle('Test title')}
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
