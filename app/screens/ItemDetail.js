e783884b7674e1ea8e829d9a4ac1a738f8745082
ermyas7 <ermyas.fekadu@gmail.com>
2021-01-22 12:19:16 +0300

    Add product tag top

--- app/screens/ItemDetail.js
+++ app/screens/ItemDetail.js
@@ -7,13 +7,16 @@ import {
   TouchableOpacity,
   Image,
 } from 'react-native';
-import {COLORS, icons, SIZES} from '../constants';
+import {COLORS, FONTS, icons, SIZES} from '../constants';
 
 const ItemDetail = ({route}) => {
   const renderHeader = () => {
     return (
       <View
-        style={{marginTop: SIZES.padding * 2, marginHorizontal: SIZES.padding}}>
+        style={{
+          marginTop: SIZES.padding * 2,
+          marginHorizontal: SIZES.padding,
+        }}>
         <View style={{flexDirection: 'row'}}>
           <TouchableOpacity
             style={{
@@ -24,7 +27,7 @@ const ItemDetail = ({route}) => {
             <Image
               source={icons.menu}
               resizeMode="contain"
-              style={{width: 25, height: 25, tinyColor: COLORS.white}}
+              style={{width: 25, height: 25, tintColor: COLORS.white}}
             />
           </TouchableOpacity>
           <TouchableOpacity
@@ -43,6 +46,58 @@ const ItemDetail = ({route}) => {
       </View>
     );
   };
+
+  const renderProductTag = () => {
+    const {itemInfo} = route.params;
+    return (
+      <>
+        {/* product tag */}
+        <View
+          style={{
+            position: 'absolute',
+            top: '45%',
+            left: '15%',
+            alignItems: 'center',
+            justifyContent: 'center',
+            width: 40,
+            height: 40,
+            borderRadius: 20,
+            backgroundColor: COLORS.transparentLightGray1,
+          }}>
+          <View
+            style={{
+              width: 10,
+              height: 10,
+              borderRadius: 5,
+              backgroundColor: COLORS.blue,
+            }}
+          />
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            top: '43%',
+            left: '30%',
+            backgroundColor: COLORS.transparentLightGray1,
+            width: '50%',
+            flexDirection: 'row',
+            padding: SIZES.radius * 1.5,
+            borderRadius: 10,
+          }}>
+          <View style={{flex: 2}}>
+            <Text style={{color: COLORS.darkGray, ...FONTS.h3}}>
+              {itemInfo.productName}
+            </Text>
+          </View>
+          <View style={{flex: 1.5, justifyContent: 'flex-end', alignItems: 'flex-end'}}>
+            <Text style={{color: COLORS.darkGreen, ...FONTS.h3}}>
+              $ {itemInfo.price.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+      </>
+    );
+  };
   const renderInfo = () => {
     const {itemInfo} = route.params;
     if (itemInfo) {
@@ -52,6 +107,7 @@ const ItemDetail = ({route}) => {
           resizeMode="cover"
           style={{width: '100%', height: '100%'}}>
           {renderHeader()}
+          {renderProductTag()}
         </ImageBackground>
       );
     }
