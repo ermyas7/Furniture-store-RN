76face698a4dcd482f7a514e37dd8f22cae36294
ermyas7 <ermyas.fekadu@gmail.com>
2021-01-22 04:38:10 +0300

    Add scrollable home tab

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -6,13 +6,180 @@ import {
   StyleSheet,
   TouchableOpacity,
   Image,
+  FlatList,
 } from 'react-native';
-import {COLORS, icons, SIZES, FONTS} from '../constants';
+import {COLORS, icons, SIZES, FONTS, images} from '../constants';
+
+const ScrollableTab = ({tabList, selectedTab, onPress}) => {
+  const renderItem = ({item}) => {
+    return (
+      <TouchableOpacity
+        style={{marginHorizontal: SIZES.padding}}
+        onPress={() => onPress(item)}>
+        <Text style={{color: COLORS.secondary, ...FONTS.body2}}>
+          {item.name}
+        </Text>
+        {selectedTab.id === item.id ? (
+          <View style={{alignItems: 'center', marginTop: SIZES.base}}>
+            <View
+              style={{
+                width: 10,
+                height: 10,
+                borderRadius: 5,
+                backgroundColor: COLORS.blue,
+              }}
+            />
+          </View>
+        ) : null}
+      </TouchableOpacity>
+    );
+  };
+  return (
+    <View>
+      <FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        data={tabList}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+};
 
 const Home = () => {
+  const [tabList, setTabList] = React.useState([
+    {
+      id: 0,
+      name: 'Chair',
+      title: 'chairs',
+      productList: [
+        {
+          productId: 1,
+          productName: 'Chair Green Colour',
+          price: 10.0,
+          image: images.greenChair,
+        },
+        {
+          productId: 2,
+          productName: 'Chair Peach Colour',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 3,
+          productName: 'Chair White Colour',
+          price: 10.0,
+          image: images.whiteChair,
+        },
+      ],
+    },
+    {
+      id: 1,
+      name: 'Cupboard',
+      title: 'cupboards',
+      productList: [
+        {
+          productId: 4,
+          productName: 'Product 4',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 5,
+          productName: 'Product 5',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 6,
+          productName: 'Product 6',
+          price: 10.0,
+          image: images.redChair,
+        },
+      ],
+    },
+    {
+      id: 2,
+      name: 'Table',
+      title: 'tables',
+      productList: [
+        {
+          productId: 7,
+          productName: 'Product 7',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 8,
+          productName: 'Product 8',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 9,
+          productName: 'Product 9',
+          price: 10.0,
+          image: images.redChair,
+        },
+      ],
+    },
+    {
+      id: 3,
+      name: 'Accessories',
+      title: 'accessories',
+      productList: [
+        {
+          productId: 10,
+          productName: 'Product 10',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 11,
+          productName: 'Product 11',
+          price: 10.0,
+          image: images.redChair,
+        },
+        {
+          productId: 12,
+          productName: 'Product 12',
+          price: 10.0,
+          image: images.redChair,
+        },
+      ],
+    },
+  ]);
+
+  const [selectedTab, setSelectedTab] = React.useState({
+    id: 0,
+    name: 'Chair',
+    title: 'chairs',
+    productList: [
+      {
+        productId: 1,
+        productName: 'Chair Green Colour',
+        price: 10.0,
+        image: images.greenChair,
+      },
+      {
+        productId: 2,
+        productName: 'Chair Peach Colour',
+        price: 10.0,
+        image: images.redChair,
+      },
+      {
+        productId: 3,
+        productName: 'Chair White Colour',
+        price: 10.0,
+        image: images.whiteChair,
+      },
+    ],
+  });
   const renderHeader = () => {
     return (
-      <View style={{paddingHorizontal: SIZES.padding}}>
+      <View
+        style={{paddingHorizontal: SIZES.padding, marginTop: SIZES.padding}}>
         <View style={{flexDirection: 'row'}}>
           <TouchableOpacity
             onPress={() => console.log('pressed')}
@@ -49,7 +216,12 @@ const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       {renderHeader()}
-      {renderTitle('Test title')}
+      {renderTitle(selectedTab.title)}
+      <ScrollableTab
+        tabList={tabList}
+        selectedTab={selectedTab}
+        onPress={(item) => setSelectedTab(item)}
+      />
     </SafeAreaView>
   );
 };
